90fbb882991fac65dfab2e4a216017941a30f6b5
haiyangwang <ocean@legenddigital.com.au>
2019-04-23 17:59:44 +0800

    无效添加

--- app/src/main/java/ai/www/mygreendao/example/NoteActivity.java
+++ app/src/main/java/ai/www/mygreendao/example/NoteActivity.java
@@ -56,8 +56,7 @@ public class NoteActivity extends AppCompatActivity {
         setUpViews();
 
         //测试
-        //测试
-        //测试
+        
         // get the note DAO
         DaoSession daoSession = ((App) getApplication()).getDaoSession();
         noteDao = daoSession.getNoteDao();
